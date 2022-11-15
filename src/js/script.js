df8b8283f830a818ff60d8ae71f90d19e1842d67
Albinator <albinamivina@gmail.com>
2022-11-15 11:42:08 +0200

    add animation to progress circles

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,23 @@
-let circle = document.querySelector('.first');
-let skills = document.querySelector('.skills');
+const main = document.querySelector('.wrapper');
 
+const scrollEvent = () => {
+    const listOfCircles = [...document.querySelectorAll('.circle')];
 
+    const dash = listOfCircles.map((el) => {
+        const style = getComputedStyle(el);
+        return style.strokeDashoffset;
+    });
 
-document.addEventListener('scroll',() => {
-    if (window.scrollTop() > skills.offset.top - 1000) {
-        circle.classList.add ('active');
+    if (main.scrollTop >= 1600) {
+        listOfCircles.forEach((el, i) => {
+            el.style.setProperty('--stroke-dashoffset', dash[i]);
+            el.classList.add('active');
+        });
+    } else {
+        listOfCircles.forEach((el) => {
+            el.classList.remove('active');
+        });
     }
-});
+  };
+
+main.addEventListener('scroll', scrollEvent);
