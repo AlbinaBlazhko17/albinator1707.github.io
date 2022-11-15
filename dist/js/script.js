9770f3b6c3e66f1407756c46856c435d969f2643
Albinator <albinamivina@gmail.com>
2022-11-15 14:38:55 +0200

    add percentage animation to progress circles

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,17 +93,31 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-const main = document.querySelector('.wrapper');
+const main = document.querySelector('.wrapper'),
+  percent = document.querySelectorAll('.skills__percent'),
+  listOfCircles = [...document.querySelectorAll('.circle')];
+let timer = [],
+  counter = 0;
+const setPerc = (value, i) => {
+  const arr = [...percent];
+  counter++;
+  arr[i].textContent = `${counter}%`;
+  if (counter == value) clearInterval(timer[i]);
+};
 const scrollEvent = () => {
-  const listOfCircles = [...document.querySelectorAll('.circle')];
   const dash = listOfCircles.map(el => {
     const style = getComputedStyle(el);
     return style.strokeDashoffset;
   });
-  if (main.scrollTop >= 1600) {
+  if (main.scrollTop > 1600) {
     listOfCircles.forEach((el, i) => {
       el.style.setProperty('--stroke-dashoffset', dash[i]);
       el.classList.add('active');
+      if (!timer[i]) {
+        timer[i] = setInterval(() => setPerc(document.querySelectorAll('.skills__percent')[i].getAttribute('data-value'), i), 100);
+        console.log('start');
+      }
+      ;
     });
   } else {
     listOfCircles.forEach(el => {
