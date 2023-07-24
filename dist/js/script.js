0d589dcab080ad7f8cc8e8fc6162393ef88289ef
Albinator <albinamivina@gmail.com>
2023-07-24 15:31:13 +0300

    fix bug

--- dist/js/script.js
+++ dist/js/script.js
@@ -166,12 +166,12 @@ buttonProjects.addEventListener("click", (e) => {
 	expandBlockEvent();
 });
 
-// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-// 	anchor.addEventListener("click", function (e) {
-// 		e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+	anchor.addEventListener("click", function (e) {
+		e.preventDefault();
 
-// 		document.querySelector(this.getAttribute("href")).scrollIntoView({
-// 			behavior: "smooth",
-// 		});
-// 	});
-// });
+		document.querySelector(this.getAttribute("href")).scrollIntoView({
+			behavior: "smooth",
+		});
+	});
+});
